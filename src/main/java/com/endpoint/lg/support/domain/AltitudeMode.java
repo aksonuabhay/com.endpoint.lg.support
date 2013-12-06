9211f7fb6f926c978cab6a4d5db14161ee1e5c89
Keith Hughes <khughes@google.com>
2013-12-06 14:51:40 -0700

    Support planets in querytxt, continue to add to support library for common messages.

---
+++ src/main/java/com/endpoint/lg/support/domain/AltitudeMode.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.endpoint.lg.support.domain;
+
+/**
+ * @author Keith M. Hughes
+ */
+public enum AltitudeMode {
+  /**
+   * Altitude is relative to the sea floor.
+   */
+  RELATIVE_TO_SEA_FLOOR("relativeToSeaFloor");
+
+  /**
+   * The value of the enum to use when speaking to Google Earth.
+   */
+  private String value;
+
+  private AltitudeMode(String value) {
+    this.value = value;
+  }
+
+  /**
+   * get the value for the string to be used.
+   *
+   * @return the value
+   */
+  public String getValue() {
+    return value;
+  }
+}
--- src/main/java/com/endpoint/lg/support/message/querytxt/MessageTypesQueryTxt.java
+++ src/main/java/com/endpoint/lg/support/message/querytxt/MessageTypesQueryTxt.java
@@ -29,12 +29,58 @@ public class MessageTypesQueryTxt {
   public static final String MESSAGE_TYPE_QUERYTXT_TOUR = "tour";
 
   /**
+   * The querytxt message for playing a tour. {@code true} to start playing,
+   * {@code false} to stop playing.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_TOUR_PLAY = "play";
+
+  /**
+   * The querytxt message for specifying the tour name of a tour.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_TOUR_TOURNAME = "tourName";
+
+  /**
    * The querytxt message for searching.
    */
   public static final String MESSAGE_TYPE_QUERYTXT_SEARCH = "search";
 
   /**
+   * The querytxt message for specifying the query for a search.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_SEARCH_QUERY = "query";
+
+  /**
+   * The querytxt message for specifying the label for a search.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_SEARCH_LABEL = "label";
+
+  /**
    * The querytxt message for flyto.
    */
   public static final String MESSAGE_TYPE_QUERYTXT_FLYTO = "flyto";
+
+  /**
+   * The querytxt field for the flyto type field.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_FLYTO_TYPE = "type";
+
+  /**
+   * The value for the flyto type parameter for getting a camera flyto.
+   */
+  public static final String MESSAGE_FIELD_VALUE_FLYTO_TYPE_CAMERA = "camera";
+
+  /**
+   * The value for the flyto type parameter for getting a lookat flyto.
+   */
+  public static final String MESSAGE_FIELD_VALUE_FLYTO_TYPE_LOOKAT = "lookat";
+
+  /**
+   * The querytxt message for planet.
+   */
+  public static final String MESSAGE_TYPE_QUERYTXT_PLANET = "planet";
+
+  /**
+   * The querytxt field for the planet destination field.
+   */
+  public static final String MESSAGE_FIELD_QUERYTXT_PLANET_DESTINATION = "destination";
 }
