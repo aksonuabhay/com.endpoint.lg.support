c88e4b58b97ee1c13801be2341dab8139259de7d
Matt Vollrath <matt@endpoint.com>
2014-05-03 20:40:43 -0400

    Refactor and fix LEAP Reader

--- src/main/java/com/endpoint/lg/support/message/GestureMessages.java
+++ src/main/java/com/endpoint/lg/support/message/GestureMessages.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.endpoint.lg.support.message;
 
 import java.util.Map;
@@ -10,17 +26,19 @@ import interactivespaces.util.geometry.Vector3;
 import interactivespaces.service.image.gesture.Gesture;
 import interactivespaces.service.image.gesture.GestureHand;
 import interactivespaces.service.image.gesture.GesturePointable;
+import interactivespaces.service.image.gesture.leapmotion.LeapMotionGestureEndpoint;
 
-import static com.endpoint.lg.support.message.DomainMessages.*;
+import static com.endpoint.lg.support.message.DomainMessages.deserializeVector3;
+import static com.endpoint.lg.support.message.DomainMessages.serializeVector3;
 
 /**
- * Create messages for gesture events.
+ * Methods for serialization of gesture events.
  * 
  * @author Matt Vollrath <matt@endpoint.com>
  */
 public class GestureMessages {
   /**
-   * Deserialize all gestures from data.
+   * Deserializes all gestures from data.
    * 
    * @param data
    *          gesture data
@@ -29,7 +47,7 @@ public class GestureMessages {
   public static Map<String, Gesture> deserializeGestures(JsonNavigator data) {
     Map<String, Gesture> gestures = Maps.newHashMap();
 
-    data.down(GestureMessageFields.FIELD_GESTURES);
+    data.down(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURES);
 
     Map<String, Object> dataGestures = data.getCurrentItem();
 
@@ -43,7 +61,7 @@ public class GestureMessages {
   }
 
   /**
-   * Deserialize a single gesture from data.
+   * Deserializes a single gesture from data.
    * 
    * @param data
    *          gesture data
@@ -54,9 +72,9 @@ public class GestureMessages {
   public static Gesture deserializeGesture(JsonNavigator data, String id) {
     data.down(id);
 
-    String type = data.getString(GestureMessageFields.FIELD_TYPE);
+    String type = data.getString(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_TYPE);
     Gesture.GestureState state = deserializeGestureState(data);
-    Double duration = data.getDouble(GestureMessageFields.FIELD_DURATION);
+    Double duration = data.getDouble(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_DURATION);
 
     data.up();
 
@@ -64,18 +82,19 @@ public class GestureMessages {
   }
 
   /**
-   * Deserialize a gesture state.
+   * Deserializes a gesture state.
    * 
    * @param data
    *          gesture data
    * @return the gesture state
    */
   public static Gesture.GestureState deserializeGestureState(JsonNavigator data) {
-    return Gesture.GestureState.valueOf(data.getString(GestureMessageFields.FIELD_STATE));
+    return Gesture.GestureState.valueOf(data
+        .getString(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_STATE));
   }
 
   /**
-   * Deserialize all hands from data.
+   * Deserializes all hands from data.
    * 
    * @param data
    *          hand data
@@ -84,12 +103,12 @@ public class GestureMessages {
   public static Map<String, GestureHand> deserializeGestureHands(JsonNavigator data) {
     Map<String, GestureHand> hands = Maps.newHashMap();
 
-    data.down(GestureMessageFields.FIELD_GESTURE_HANDS);
+    data.down(LeapMotionGestureEndpoint.LEAPMOTION_NAME_HANDS);
 
     Map<String, Object> dataHands = data.getCurrentItem();
 
     for (String id : dataHands.keySet()) {
-      deserializeGestureHand(data, id);
+      hands.put(id, deserializeGestureHand(data, id));
     }
 
     data.up();
@@ -98,7 +117,7 @@ public class GestureMessages {
   }
 
   /**
-   * Deserialize a single hand from data.
+   * Deserializes a single hand from data.
    * 
    * @param data
    *          hand data
@@ -109,12 +128,18 @@ public class GestureMessages {
   public static GestureHand deserializeGestureHand(JsonNavigator data, String id) {
     data.down(id);
 
-    Vector3 direction = deserializeVector3(data, GestureMessageFields.FIELD_DIRECTION);
-    Vector3 palmNormal = deserializeVector3(data, GestureMessageFields.FIELD_PALM_NORMAL);
-    Vector3 palmPosition = deserializeVector3(data, GestureMessageFields.FIELD_PALM_POSITION);
-    Vector3 palmVelocity = deserializeVector3(data, GestureMessageFields.FIELD_PALM_VELOCITY);
-    Vector3 sphereCenter = deserializeVector3(data, GestureMessageFields.FIELD_SPHERE_CENTER);
-    Double sphereRadius = data.getDouble(GestureMessageFields.FIELD_SPHERE_RADIUS);
+    Vector3 direction =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_DIRECTION);
+    Vector3 palmNormal =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_NORMAL);
+    Vector3 palmPosition =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_POSITION);
+    Vector3 palmVelocity =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_VELOCITY);
+    Vector3 sphereCenter =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_SPHERE_CENTER);
+    Double sphereRadius =
+        data.getDouble(LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_SPHERE_RADIUS);
 
     data.up();
 
@@ -123,7 +148,7 @@ public class GestureMessages {
   }
 
   /**
-   * Deserialize all pointables from data.
+   * Deserializes all pointables from data.
    * 
    * @param data
    *          pointable data
@@ -132,7 +157,7 @@ public class GestureMessages {
   public static Map<String, GesturePointable> deserializeGesturePointables(JsonNavigator data) {
     Map<String, GesturePointable> pointables = Maps.newHashMap();
 
-    data.down(GestureMessageFields.FIELD_GESTURE_POINTABLES);
+    data.down(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLES);
 
     Map<String, Object> dataPointables = data.getCurrentItem();
 
@@ -146,7 +171,7 @@ public class GestureMessages {
   }
 
   /**
-   * Deserialize a single pointable from data.
+   * Deserializes a single pointable from data.
    * 
    * @param data
    *          pointable data
@@ -157,11 +182,14 @@ public class GestureMessages {
   public static GesturePointable deserializeGesturePointable(JsonNavigator data, String id) {
     data.down(id);
 
-    Vector3 direction = deserializeVector3(data, GestureMessageFields.FIELD_DIRECTION);
-    Double length = data.getDouble(GestureMessageFields.FIELD_LENGTH);
-    Vector3 tipPosition = deserializeVector3(data, GestureMessageFields.FIELD_TIP_POSITION);
-    Vector3 tipVelocity = deserializeVector3(data, GestureMessageFields.FIELD_TIP_VELOCITY);
-    Boolean isTool = data.getBoolean(GestureMessageFields.FIELD_IS_TOOL);
+    Vector3 direction =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_DIRECTION);
+    Double length = data.getDouble(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_LENGTH);
+    Vector3 tipPosition =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TIP_POSITION);
+    Vector3 tipVelocity =
+        deserializeVector3(data, LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TIP_VELOCITY);
+    Boolean isTool = data.getBoolean(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TOOL);
 
     data.up();
 
@@ -169,7 +197,7 @@ public class GestureMessages {
   }
 
   /**
-   * Serialize a collection of gestures to data.
+   * Serializes a collection of gestures to data.
    * 
    * @param gestures
    *          the gestures to serialize
@@ -177,7 +205,7 @@ public class GestureMessages {
    *          the data to write to
    */
   public static void serializeGestures(Map<String, Gesture> gestures, JsonBuilder data) {
-    data.newObject(GestureMessageFields.FIELD_GESTURES);
+    data.newObject(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURES);
 
     for (String id : gestures.keySet()) {
       serializeGesture(gestures.get(id), data);
@@ -187,7 +215,7 @@ public class GestureMessages {
   }
 
   /**
-   * Serialize a single gesture to data.
+   * Serializes a single gesture to data.
    * 
    * @param gesture
    *          the gesture to serialize
@@ -199,16 +227,16 @@ public class GestureMessages {
 
     data.newObject(id);
 
-    data.put(GestureMessageFields.FIELD_ID, id);
-    data.put(GestureMessageFields.FIELD_TYPE, gesture.getType());
-    data.put(GestureMessageFields.FIELD_DURATION, gesture.getDuration());
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_ID, id);
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_TYPE, gesture.getType());
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_DURATION, gesture.getDuration());
     serializeGestureState(gesture.getState(), data);
 
     data.up();
   }
 
   /**
-   * Serialize a gesture state.
+   * Serializes a gesture state.
    * 
    * @param state
    *          the state to serialize
@@ -216,11 +244,11 @@ public class GestureMessages {
    *          the data to write to
    */
   public static void serializeGestureState(Gesture.GestureState state, JsonBuilder data) {
-    data.put(GestureMessageFields.FIELD_STATE, state.toString());
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_GESTURE_STATE, state.toString());
   }
 
   /**
-   * Serialize a collection of hands to data.
+   * Serializes a collection of hands to data.
    * 
    * @param hands
    *          the hands to serialize
@@ -228,7 +256,7 @@ public class GestureMessages {
    *          the data to write to
    */
   public static void serializeGestureHands(Map<String, GestureHand> hands, JsonBuilder data) {
-    data.newObject(GestureMessageFields.FIELD_GESTURE_HANDS);
+    data.newObject(LeapMotionGestureEndpoint.LEAPMOTION_NAME_HANDS);
 
     for (String id : hands.keySet()) {
       serializeGestureHand(hands.get(id), data);
@@ -238,7 +266,7 @@ public class GestureMessages {
   }
 
   /**
-   * Serialize a hand to data.
+   * Serializes a hand to data.
    * 
    * @param hand
    *          the hand to serialize
@@ -250,19 +278,24 @@ public class GestureMessages {
 
     data.newObject(id);
 
-    data.put(GestureMessageFields.FIELD_ID, id);
-    serializeVector3(hand.getPalmPosition(), GestureMessageFields.FIELD_PALM_POSITION, data);
-    serializeVector3(hand.getPalmVelocity(), GestureMessageFields.FIELD_PALM_VELOCITY, data);
-    serializeVector3(hand.getPalmNormal(), GestureMessageFields.FIELD_PALM_NORMAL, data);
-    serializeVector3(hand.getSphereCenter(), GestureMessageFields.FIELD_SPHERE_CENTER, data);
-    serializeVector3(hand.getDirection(), GestureMessageFields.FIELD_DIRECTION, data);
-    data.put(GestureMessageFields.FIELD_SPHERE_RADIUS, hand.getSphereRadius());
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_ID, id);
+    serializeVector3(hand.getPalmPosition(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_POSITION, data);
+    serializeVector3(hand.getPalmVelocity(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_VELOCITY, data);
+    serializeVector3(hand.getPalmNormal(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_PALM_NORMAL, data);
+    serializeVector3(hand.getSphereCenter(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_SPHERE_CENTER, data);
+    serializeVector3(hand.getDirection(), LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_DIRECTION,
+        data);
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_HAND_SPHERE_RADIUS, hand.getSphereRadius());
 
     data.up();
   }
 
   /**
-   * Serialize a collection of pointables to data.
+   * Serializes a collection of pointables to data.
    * 
    * @param pointables
    *          the pointables to serialize
@@ -271,7 +304,7 @@ public class GestureMessages {
    */
   public static void serializeGesturePointables(Map<String, GesturePointable> pointables,
       JsonBuilder data) {
-    data.newObject(GestureMessageFields.FIELD_GESTURE_POINTABLES);
+    data.newObject(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLES);
 
     for (String id : pointables.keySet()) {
       serializeGesturePointable(pointables.get(id), data);
@@ -281,7 +314,7 @@ public class GestureMessages {
   }
 
   /**
-   * Serialize a single pointable to data.
+   * Serializes a single pointable to data.
    * 
    * @param pointable
    *          the pointable to be serialized
@@ -293,12 +326,15 @@ public class GestureMessages {
 
     data.newObject(id);
 
-    data.put(GestureMessageFields.FIELD_ID, id);
-    data.put(GestureMessageFields.FIELD_LENGTH, pointable.getLength());
-    data.put(GestureMessageFields.FIELD_IS_TOOL, pointable.isTool());
-    serializeVector3(pointable.getDirection(), GestureMessageFields.FIELD_DIRECTION, data);
-    serializeVector3(pointable.getTipPosition(), GestureMessageFields.FIELD_TIP_POSITION, data);
-    serializeVector3(pointable.getTipVelocity(), GestureMessageFields.FIELD_TIP_VELOCITY, data);
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_ID, id);
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_LENGTH, pointable.getLength());
+    data.put(LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TOOL, pointable.isTool());
+    serializeVector3(pointable.getDirection(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_DIRECTION, data);
+    serializeVector3(pointable.getTipPosition(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TIP_POSITION, data);
+    serializeVector3(pointable.getTipVelocity(),
+        LeapMotionGestureEndpoint.LEAPMOTION_NAME_POINTABLE_TIP_VELOCITY, data);
 
     data.up();
   }
